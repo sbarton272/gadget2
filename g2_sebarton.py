069fdc0b81018037f01d09667be0f6f69ac2669f
sbarton272 <sbarton272@gmail.com>
2013-05-07 00:42:23 -0400

    alarm sound added, need to work on diff function

--- g2_sebarton.py
+++ g2_sebarton.py
@@ -6,18 +6,26 @@
 # read from serial, analyze and active alarm
 # serial from http://pyserial.sourceforge.net/
 
-import serial, time, re, sys
+import serial, time, re, sys, winsound
 import lookupTable as lookup
 
 port = 6 # COM 7
 baud = 9600
 timeout = 3 # sec
+MAXLINE = 32
+(a, b, c) = (.2, .4, .4)
+
+class ALARM(object):
+
+	def soundAlarm( self, duration = 10000):
+		winsound.Beep(4400, duration)
 
 class ACCELEROMETER(object):
 	
-	def __init__(self):
+	def __init__(self, abc):
 		self.ser = self.initConn()
 		self.XYZ = []
+		self.abc = abc # (a, b, c)
 
 	def X(self, time):
 		return self.XYZ[time][0]
@@ -27,6 +35,15 @@ class ACCELEROMETER(object):
 
 	def Z(self, time):
 		return self.XYZ[time][3]
+
+	def diff(self, time):
+		return self.XYZ[time][4]
+
+	def getDiff(self, data):
+		# y[n] = a*x[n]+b*x[n-1]+c*x[n-3]
+		(a, b, c) = self.abc
+		time = len(data) # @TODO
+		return 
 
 	def blinkLED(self, delay):
 		print "Blinking LED with delay of %d sec" % delay
@@ -104,34 +121,43 @@ class ACCELEROMETER(object):
 			Z = line[2]
 			formatted = "%r\t%r\t%r\t%r\t%r\t%r\t%r\t%r\t%r\n" % ( X[0], X[1], X[2], Y[0], Y[1], Y[2], Z[0], Z[1], Z[2])
 
+			# replace None with empty
+			formatted = formatted.replace("None","")
+
 			fout.write( formatted )
 
 		fout.close()
+		print "Data written to ", filename
 
 
 def main():
 	
 	print "Starting HALT Laptop Security"
 	
-	gadget = ACCELEROMETER();
+	gadget = ACCELEROMETER( (a,b,c) );
+	alarm = ALARM();
 
 	gadget.blinkLED(1)
 
 
-
-	for i in xrange(1,10):
+	i = 0
+	while i < 100:
 		gadget.ser.write("x") # request data
 
-		read = gadget.ser.readline(26)
+		read = gadget.ser.readline(MAXLINE)
+
 		if read != "\n":
 			(X,Y,Z) = gadget.parseXYZ(read)
-			print (X,Y,Z)
 
-			# write to file
+			#if gadget.motionDetected():
+			#	print "MOTION"
+			
+			i += 1
 
 	gadget.closeConn()
 	gadget.dataWrite("xyzData.txt")
 
+	alarm.soundAlarm()
 
 if __name__ == "__main__":
 	main()
